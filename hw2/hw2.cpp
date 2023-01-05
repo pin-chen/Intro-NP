a0226d85b11d35b0c7b2cc7521b899c11040ce9d
pin-chen <hardymike6308@gmail.com>
2023-01-05 15:15:54 +0800

    feat: hw2 demo

--- hw2/hw2.cpp
+++ hw2/hw2.cpp
@@ -21,35 +21,21 @@
 #define ERRORCODE -1
 #define err_quit(m) { perror(m); exit(-1); }
 
-#define DEBUG
+//#define DEBUG
 
 using namespace std;
 
-struct soa{
-	int32_t ttl;
-	uint16_t rclass;
-	uint16_t rtype;
-};
+typedef struct soa_header{
+	uint32_t serial;
+	uint32_t refresh;
+	uint32_t retry;
+	uint32_t expire;
+	uint32_t minimum;
+}__attribute__((packed)) soa;
 
-struct ns{
-	string tmp;
-};
-
-struct mx{
-	string tmp;
-};
-
-struct a{
-	string tmp;
-};
-
-struct cname{
-	string tmp;
-};
-
-struct aaaa{
-	string tmp;
-};
+typedef struct mx_header{
+	uint16_t preference;
+}__attribute__((packed)) mx;
 
 struct per_data{
 	string host;
@@ -165,6 +151,8 @@ void splitArgument(vector<string> &dst, string &src, char ccc){
 				arg.clear();
 				charType = 0;
 			}
+		}else if(src[cur] == '\r' || src[cur] == '\n'){
+			continue;
 		}else{
 			if(charType == 0) charType = 1;
 			arg.push_back(src[cur]);
@@ -238,7 +226,7 @@ void question_parser(struct query &tmp, question *q){
 	tmp.qclass = ntohs(q->qclass);
 }
 
-void parser(struct packet&tmp ,unsigned char *buf){
+int parser(struct packet&tmp ,unsigned char *buf){
 	int index = 0;
 	header_parser(tmp, (dns*)buf);
 	index += sizeof(dns);
@@ -258,6 +246,7 @@ void parser(struct packet&tmp ,unsigned char *buf){
 		cout << "qclass: " << tmp.questions[i].qclass << '\n';
 #endif
 	}
+	return index;
 }
 
 void forward(int &sockfd, struct sockaddr_in csin, unsigned char *buf, int len){
@@ -278,7 +267,9 @@ void forward(int &sockfd, struct sockaddr_in csin, unsigned char *buf, int len){
 		e = sendto(clifd, buf, len, 0, (struct sockaddr*)&sin, sizeof(sin));
 		if(e == ERRORCODE) err_quit("send to");
 		len = recvfrom(clifd, buf, MAXLINE, 0, (struct sockaddr*) &sin, &sinlen);
+#ifdef DEBUG
 		cout << "forward len: " << len << '\n';
+#endif
 		if(len == ERRORCODE) err_quit("recv");
 		dns *DNS = (dns*) buf;
 		DNS->aa = 0;
@@ -291,8 +282,395 @@ void forward(int &sockfd, struct sockaddr_in csin, unsigned char *buf, int len){
 	}
 }
 
-bool domain_check(struct packet&tmp){
-	
+string Reverse(string tmp){
+	int len = tmp.size();
+	for(int i = 0; i < len / 2; i++){
+		char t = tmp[i];
+		tmp[i] = tmp[len - i - 1];
+		tmp[len - i - 1] = t;
+	}
+	return tmp;
+}
+
+string domain_check(struct packet&tmp){
+	string qname = Reverse(tmp.questions[0].qname);
+	for(auto d : dnsInfo){
+		string dname = Reverse(d.first);
+		int len = qname.size() < dname.size() ? qname.size() : dname.size();
+		bool same = true;
+		for(int i = 0; i < len; i++){
+			if(qname[i] != dname[i]){
+				same = false;
+				break;
+			}
+		}
+		if(same) return d.first;
+	}
+	return "";
+}
+
+void strEncode(unsigned char *buf, string tmp){
+#ifdef DEBUG
+	cout << "string: " << tmp << '\n';
+	cout << "size: " << tmp.size() << '\n';
+#endif
+	int index = 0, i = 0, j = 0;
+	while(index < tmp.size()){
+		int num = 0;
+		for(j = index; j < tmp.size() && tmp[j] != '.'; j++) num++;
+		buf[i++] = num;
+		while(num-- > 0){
+			buf[i++] = tmp[index++];
+		}
+		index++;
+	}
+	buf[i++] = 0;
+#ifdef DEBUG
+	cout << "name: ";
+	for(int k = 0; k < i; k++){
+		if(buf[k] < 30) cout << '\n' << int(buf[k]) << '\n';
+		else cout << buf[k];
+	}
+	cout << "\n";
+#endif
+}
+
+void make_soa_rr(string name, per_data &soa_data, unsigned char *buf, int &index){
+	strEncode((unsigned char *)&buf[index], name);
+	index += name.size() + 1;
+	answer *ANSWER = (answer*)&buf[index];
+	ANSWER->atype = htons(SOA);
+	ANSWER->aclass = htons(IN);
+	ANSWER->ttl = htonl(stoi(soa_data.ttl.c_str()));
+	index += sizeof(answer);
+	int len = index;
+	vector<string> soa_tmp;
+	splitArgument(soa_tmp, soa_data.payload, ' ');
+	strEncode((unsigned char *)&buf[index], soa_tmp[0]);
+	index += soa_tmp[0].size() + 1;
+	strEncode((unsigned char *)&buf[index], soa_tmp[1]);
+	index += soa_tmp[1].size() + 1;
+	soa *_SOA = (soa*)&buf[index];
+	_SOA->serial = htonl(stoi(soa_tmp[2].c_str()));
+	_SOA->refresh = htonl(stoi(soa_tmp[3].c_str()));
+	_SOA->retry = htonl(stoi(soa_tmp[4].c_str()));
+	_SOA->expire = htonl(stoi(soa_tmp[5].c_str()));
+	_SOA->minimum = htonl(stoi(soa_tmp[6].c_str()));
+	index += sizeof(soa);
+	ANSWER->rdlength = htons(index - len);
+}
+
+void make_ns_rr(string name, per_data &ns_data, unsigned char *buf, int &index){
+	strEncode((unsigned char *)&buf[index], name);
+	index += name.size() + 1;
+	answer *ANSWER = (answer*)&buf[index];
+	ANSWER->atype = htons(NS);
+	ANSWER->aclass = htons(IN);
+	ANSWER->ttl = htonl(stoi(ns_data.ttl.c_str()));
+	index += sizeof(answer);
+	strEncode((unsigned char *)&buf[index], ns_data.payload);
+	index += ns_data.payload.size() + 1;
+	ANSWER->rdlength = htons(ns_data.payload.size() + 1);
+}
+
+void make_mx_rr(string name, per_data &mx_data, unsigned char *buf, int &index){
+	strEncode((unsigned char *)&buf[index], name);
+	index += name.size() + 1;
+	answer *ANSWER = (answer*)&buf[index];
+	ANSWER->atype = htons(MX);
+	ANSWER->aclass = htons(IN);
+	ANSWER->ttl = htonl(stoi(mx_data.ttl.c_str()));
+	index += sizeof(answer);
+	int len = index;
+	vector<string> mx_tmp;
+	splitArgument(mx_tmp, mx_data.payload, ' ');
+	mx *_MX = (mx*)&buf[index];
+	_MX->preference = htons(stoi(mx_tmp[0].c_str()));
+	index += sizeof(mx);
+	strEncode((unsigned char *)&buf[index], mx_tmp[1]);
+	index += mx_tmp[1].size() + 1;
+	ANSWER->rdlength = htons(index - len);
+}
+
+void make_txt_rr(string name, per_data &txt_data, unsigned char *buf, int &index){
+	strEncode((unsigned char *)&buf[index], name);
+	index += name.size() + 1;
+	answer *ANSWER = (answer*)&buf[index];
+	ANSWER->atype = htons(TXT);
+	ANSWER->aclass = htons(IN);
+	ANSWER->ttl = htonl(stoi(txt_data.ttl.c_str()));
+	index += sizeof(answer);
+	unsigned char *str = (unsigned char *)&buf[index];
+	str[0] = txt_data.payload.size() - 2;
+	for(int i = 1; i < txt_data.payload.size() - 1; i++){
+		str[i] = txt_data.payload[i];
+	}
+	index += txt_data.payload.size() - 1;
+	ANSWER->rdlength = htons(txt_data.payload.size() - 1);
+}
+
+void make_cname_rr(string name, per_data &cname_data, unsigned char *buf, int &index){
+	strEncode((unsigned char *)&buf[index], name);
+	index += name.size() + 1;
+	answer *ANSWER = (answer*)&buf[index];
+	ANSWER->atype = htons(CNAME);
+	ANSWER->aclass = htons(IN);
+	ANSWER->ttl = htonl(stoi(cname_data.ttl.c_str()));
+	index += sizeof(answer);
+	strEncode((unsigned char *)&buf[index], cname_data.payload);
+	index += cname_data.payload.size() + 1;
+	ANSWER->rdlength = htons(cname_data.payload.size() + 1);
+}
+
+void make_a_rr(string name, per_data &a_data, unsigned char *buf, int &index){
+	strEncode((unsigned char *)&buf[index], name);
+	index += name.size() + 1;
+	answer *ANSWER = (answer*)&buf[index];
+	ANSWER->atype = htons(A);
+	ANSWER->aclass = htons(IN);
+	ANSWER->ttl = htonl(stoi(a_data.ttl.c_str()));
+	index += sizeof(answer);
+	inet_pton(AF_INET, a_data.payload.c_str(), (in_addr *)&buf[index]);
+	index += 4;
+	ANSWER->rdlength = htons(4);
+}
+
+void make_aaaa_rr(string name, per_data &aaaa_data, unsigned char *buf, int &index){
+	strEncode((unsigned char *)&buf[index], name);
+	index += name.size() + 1;
+	answer *ANSWER = (answer*)&buf[index];
+	ANSWER->atype = htons(AAAA);
+	ANSWER->aclass = htons(IN);
+	ANSWER->ttl = htonl(stoi(aaaa_data.ttl.c_str()));
+	index += sizeof(answer);
+	inet_pton(AF_INET6, aaaa_data.payload.c_str(), (in_addr *)&buf[index]);
+	index += 16;
+	ANSWER->rdlength = htons(16);
+}
+
+int make_packet(string domain, struct packet&tmp, unsigned char *buf, int index){
+	dns *DNS = (dns*)buf;
+	DNS->qr = 1;
+	DNS->aa = 1;
+	DNS->ra = 1;
+	DNS->qdcount = htons(1);
+	uint16_t anc = 0, nsc = 0, arc = 0;
+	switch(tmp.questions[0].qtype){
+		case A:{
+			vector<string> c_name;
+			for(auto r : dnsInfo[domain].records){
+				if(r.rtype == "CNAME"){
+					string host = "";
+					if(r.host[r.host.size() - 1] == '.'){
+						host = r.host;
+					}else{
+						host = r.host + "." + domain;
+					}
+					if(host != tmp.questions[0].qname) continue;
+					anc++;
+					c_name.push_back(r.payload);
+					make_cname_rr(tmp.questions[0].qname, r, buf, index);
+				}
+			}
+			for(auto r : dnsInfo[domain].records){
+				if(r.rtype == "A"){
+					string host = "";
+					if(r.host[r.host.size() - 1] == '.'){
+						host = r.host;
+					}else{
+						host = r.host + "." + domain;
+					}
+					if(host != tmp.questions[0].qname) continue;
+					anc++;
+					make_a_rr(host, r, buf, index);
+				}
+			}
+			for(auto qname : c_name){
+				for(auto r : dnsInfo[domain].records){
+					if(r.rtype == "A"){
+						string host = "";
+						if(r.host[r.host.size() - 1] == '.'){
+							host = r.host;
+						}else{
+							host = r.host + "." + domain;
+						}
+						if(host != qname) continue;
+						anc++;
+						make_a_rr(host, r, buf, index);
+					}
+				}
+			}
+			if(anc == 0) break;
+			for(auto r : dnsInfo[domain].records){
+				if(r.rtype == "NS"){
+					nsc++;
+					make_ns_rr(domain, r, buf, index);
+				}
+			}
+			break;
+		}
+		case NS:{
+			vector<string> ns_name;
+			for(auto r : dnsInfo[domain].records){
+				if(r.rtype == "NS"){
+					anc++;
+					ns_name.push_back(r.payload);
+					make_ns_rr(domain, r, buf, index);
+				}
+			}
+			for(auto qname : ns_name){
+				for(auto r : dnsInfo[domain].records){
+					if(r.rtype == "A"){
+						string host = "";
+						if(r.host[r.host.size() - 1] == '.'){
+							host = r.host;
+						}else{
+							host = r.host + "." + domain;
+						}
+						if(host != qname) continue;
+						arc++;
+						make_a_rr(host, r, buf, index);
+					}
+				}
+			}
+			break;
+		}
+		case MX:{
+			vector<string> mx_name;
+			for(auto r : dnsInfo[domain].records){
+				if(r.rtype == "MX"){
+					anc++;
+					vector<string> temp;
+					splitArgument(temp, r.payload, ' ');
+					mx_name.push_back(temp[1]);
+					make_mx_rr(domain, r, buf, index);
+				}
+			}
+			for(auto r : dnsInfo[domain].records){
+				if(r.rtype == "NS"){
+					nsc++;
+					make_ns_rr(domain, r, buf, index);
+				}
+			}
+			for(auto qname : mx_name){
+				for(auto r : dnsInfo[domain].records){
+					if(r.rtype == "A"){
+						string host = "";
+						if(r.host[r.host.size() - 1] == '.'){
+							host = r.host;
+						}else{
+							host = r.host + "." + domain;
+						}
+						if(host != qname) continue;
+						arc++;
+						make_a_rr(host, r, buf, index);
+					}
+				}
+			}
+			break;
+		}
+		case SOA:{
+			for(auto r : dnsInfo[domain].records){
+				if(r.rtype == "SOA"){
+					anc++;
+					make_soa_rr(domain, r, buf, index);
+					break;
+				}
+			}
+			break;
+		}
+		case CNAME:{
+			for(auto r : dnsInfo[domain].records){
+				if(r.rtype == "CNAME"){
+					string host = "";
+					if(r.host[r.host.size() - 1] == '.'){
+						host = r.host;
+					}else{
+						host = r.host + "." + domain;
+					}
+					if(host != tmp.questions[0].qname) continue;
+					anc++;
+					make_cname_rr(tmp.questions[0].qname, r, buf, index);
+				}
+			}
+			break;
+		}
+		case TXT:{
+			for(auto r : dnsInfo[domain].records){
+				if(r.rtype == "TXT"){
+					anc++;
+					make_txt_rr(domain, r, buf, index);
+				}
+			}
+			break;
+		}
+		case AAAA:{
+			vector<string> c_name;
+			for(auto r : dnsInfo[domain].records){
+				if(r.rtype == "CNAME"){
+					string host = "";
+					if(r.host[r.host.size() - 1] == '.'){
+						host = r.host;
+					}else{
+						host = r.host + "." + domain;
+					}
+					if(host != tmp.questions[0].qname) continue;
+					anc++;
+					c_name.push_back(r.payload);
+					make_cname_rr(tmp.questions[0].qname, r, buf, index);
+				}
+			}
+			for(auto r : dnsInfo[domain].records){
+				if(r.rtype == "AAAA"){
+					string host = "";
+					if(r.host[r.host.size() - 1] == '.'){
+						host = r.host;
+					}else{
+						host = r.host + "." + domain;
+					}
+					if(host != tmp.questions[0].qname) continue;
+					anc++;
+					make_aaaa_rr(host, r, buf, index);
+				}
+			}
+			for(auto qname : c_name){
+				for(auto r : dnsInfo[domain].records){
+					if(r.rtype == "AAAA"){
+						string host = "";
+						if(r.host[r.host.size() - 1] == '.'){
+							host = r.host;
+						}else{
+							host = r.host + "." + domain;
+						}
+						if(host != qname) continue;
+						anc++;
+						make_aaaa_rr(host, r, buf, index);
+					}
+				}
+			}
+			if(anc == 0) break;
+			for(auto r : dnsInfo[domain].records){
+				if(r.rtype == "NS"){
+					nsc++;
+					make_ns_rr(domain, r, buf, index);
+				}
+			}
+			break;
+		}
+	}
+	if(anc == 0){
+		for(auto r : dnsInfo[domain].records){
+			if(r.rtype == "SOA"){
+				nsc++;
+				make_soa_rr(domain, r, buf, index);
+				break;
+			}
+		}
+	}
+	DNS->ancount = htons(anc);
+	DNS->nscount = htons(nsc);
+	DNS->arcount = htons(arc);
+	return index;
 }
 
 void run(int port){
@@ -314,10 +692,44 @@ void run(int port){
 #endif
 		//parse
 		struct packet request;
-		parser(request, buf);
+		int index = parser(request, buf);
 		//check own domain
-		if(domain_check(request)){//send resonse
-			
+		string domain = domain_check(request);
+		
+		if(domain != ""){//send resonse
+			regex reg("^([0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3})\\.([0-9a-zA-Z]{1,61}\\.)*");
+			if(regex_match(request.questions[0].qname, reg)){
+				dns *DNS = (dns*)buf;
+				DNS->qr = 1;
+				DNS->aa = 1;
+				DNS->ra = 1;
+				DNS->qdcount = htons(1);
+				DNS->ancount = htons(1);
+				DNS->nscount = 0;
+				DNS->arcount = 0;
+				strEncode((unsigned char *)&buf[index], request.questions[0].qname);
+				index += request.questions[0].qname.size() + 1;
+				answer *ANSWER = (answer*)&buf[index];
+				ANSWER->atype = htons(A);
+				ANSWER->aclass = htons(IN);
+				ANSWER->ttl = htonl(1);
+				index += sizeof(answer);
+				int iip;
+				int cip = 0;
+				for(iip = 0; iip < request.questions[0].qname.size(); iip++){
+					if(request.questions[0].qname[iip] == '.') cip++;
+					if(cip > 3) break;
+				}
+				inet_pton(AF_INET, request.questions[0].qname.substr(0, iip).c_str(), (in_addr *)&buf[index]);
+				index += 4;
+				ANSWER->rdlength = htons(4);
+			}else{
+				index = make_packet(domain, request, buf, index);
+			}
+#ifdef DEBUG
+			cout << "check domain: " << domain << '\n';
+#endif
+			sendto(sockfd, buf, index, 0, (struct sockaddr*)&csin, sizeof(csin));
 		}else{//forward
 			forward(sockfd, csin, buf, len);
 		}
